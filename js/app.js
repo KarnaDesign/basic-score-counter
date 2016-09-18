3f046734c2bdef44c46364988a5425709de59653
Ida Ferm <ida.kg.ferm@gmail.com>
2016-09-18 16:48:42 +0200

    Added styling for page and counter

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 function Header(props) {
-    return(
+    return (
         <header>
             <h1>{props.title}</h1>
             <p>{props.tagLine}</p>
@@ -8,11 +8,37 @@ function Header(props) {
 }
 
 function Application(props) {
-    return(
+    return (
         <div id="app">
             <Header title={props.title} tagLine={props.tagLine}/>
+            <div id="scoreSheet">
+                <div className="row">
+                    <div className="left col">
+                        <h4>Player</h4>
+                    </div>
+                    <div className="right col">
+                        <h4>Score</h4>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="left col">
+                        <input type="text" placeholder="Player name"/>
+                    </div>
+                    <div className="right col">
+                        <div className="counter">
+                            <button className="plus">+</button>
+                            <input type="number"/>
+                            <button className="minus">-</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
 
-ReactDOM.render(<Application title="Basic Score Counter" tagLine="Keeping track!"/>, document.getElementById("wrapper"));
+ReactDOM.render(
+    <Application title="Basic Score Counter"
+                 tagLine="Keeping track!"/>
+    , document.getElementById("wrapper"));
